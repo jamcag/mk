86d8e0419ef3c5755503ddb0c669f529ed3d4029
JC <jamcag@proton.me>
2023-02-06 02:17:19 -0500

    Create mk app

---
+++ mk.cpp
@@ -0,0 +1,22 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+int main(int argc, char** argv) {
+    if (argc == 1 || argc > 2) {
+        std::cerr << "error: Needs one argument.";
+        return EXIT_FAILURE;
+    }
+    const char* name = argv[1];
+
+    std::string command = "g++ -std=c++20 ";
+    command += name;
+    command += ".cpp ";
+    command += "-o ";
+    command += name;
+    int ret = std::system(command.c_str());
+    if (ret) {
+        std::cerr << "error: Compilation failed";
+        return EXIT_FAILURE;
+    }
+}
